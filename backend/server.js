7e73f70f43cb5d6b19324ad33ec80552845bfd79
lakshitha-madushan-randoli <lakshitha.madushan@randoli.ca>
2025-09-07 21:11:08 +0530

    second

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
 const app = require('./app');
 const connectDB = require('./app/config/db');
+const fs = require('fs');
+const path = require('path');
+const https = require('https');
 
 
 
@@ -9,9 +12,16 @@ const PORT = process.env.PORT || 5000;
 // Connect to MongoDB
 connectDB();
 
-const server = app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+
+const options = {
+  key: fs.readFileSync('./cert/key.key'),
+  cert: fs.readFileSync('./cert/cert.crt'),
+};
+
+https.createServer(options, app).listen(PORT, () => {
+  console.log(`HTTPS running on https://localhost:${PORT}`);
 });
+
 
 // Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  API_BASE_URL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000",
+  API_BASE_URL: import.meta.env.VITE_API_BASE_URL || "https://localhost:5000",
   REACT_APP_AUTH0_DOMAIN:import.meta.env.REACT_APP_AUTH0_DOMAIN || "dev-jbknh2mwlfpjcfsd.us.auth0.com",
   REACT_APP_AUTH0_CLIENT_ID:import.meta.env.REACT_APP_AUTH0_CLIENT_ID || "rbiWrEk3jaS43u6YFyyZ5H65eVaDsF6h",
   REACT_APP_AUTH0_AUDIENCE:import.meta.env.REACT_APP_AUTH0_AUDIENCE || "http://localhost:5000/api",
